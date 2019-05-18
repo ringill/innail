202cda6b5bc5e69569cd9681f29a5bfecda149aa
ringill-ci-build <ringill@mail.ru>
2019-05-18 16:34:42 +0000

    Updates

---
+++ precache-manifest.ab1123f01b1781a4cbd89d4bac07fff9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df570f24383021594357f77ecada5aa9",
+    "url": "//innail.info/index.html"
+  },
+  {
+    "revision": "c50adcdb026858c569e6",
+    "url": "//innail.info/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "//innail.info/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c50adcdb026858c569e6",
+    "url": "//innail.info/static/js/main.3b84e904.chunk.js"
+  },
+  {
+    "revision": "4138cdeeda692f005c79",
+    "url": "//innail.info/static/js/runtime~main.9e3e40e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "//innail.info/static/media/logo.5d5d9eef.svg"
+  }
+]);
