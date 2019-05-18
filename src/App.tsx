91e6a1562a2367e24f86a67648fd6ebb5e2eee81
Mikhail Guskov <ringill@mail.ru>
2019-05-18 17:35:58 +0300

    add check message

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <h4>check message</h4>
         <a
           className="App-link"
           href="https://reactjs.org"
