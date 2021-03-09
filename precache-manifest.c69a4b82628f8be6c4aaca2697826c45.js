28969bd1782a8e00d9fafdc9bd48329d13bf6404
ringill-ci-build <ringill@mail.ru>
2021-03-09 04:02:50 +0000

    Updates

---
+++ precache-manifest.c69a4b82628f8be6c4aaca2697826c45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1800fcf01ec663b8f09b1040dfdccca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeff6b390a577979a1c2",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a26d206ae5171ced63c7",
+    "url": "/static/css/main.d10e0c53.chunk.css"
+  },
+  {
+    "revision": "eeff6b390a577979a1c2",
+    "url": "/static/js/2.c0630583.chunk.js"
+  },
+  {
+    "revision": "a26d206ae5171ced63c7",
+    "url": "/static/js/main.06e3a8f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "713866e7c710f1309b5cc6167efcfe5d",
+    "url": "/static/media/contacts.713866e7.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
